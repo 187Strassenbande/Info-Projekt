9b131fcf8db2769421ca3b44662912c1e7da223b
Marius <marius.albrecht@freenet.de>
2021-08-14 16:50:17 +0200

    reset

--- README.md
+++
@@ -1,2 +0,0 @@
-# Info Projekt
- diesdas
--- start.py
+++
@@ -1,9 +0,0 @@
-from manimlib import *
-
-class SquareToCircle(Scene):
-    def construct(self):
-        square = Square()
-        square.set_fill(BLUE, opacity=0.1)
-        square.set_stroke(BLUE_E, width=3)
-
-        self.play(ShowCreation(square))
